1459ec83304b74eae392c769fcd46a3b2771230f
Vasudev Jaiswal <vasujaiswal00@gmail.com>
2021-07-26 21:15:56 +0530

    Python.py

---
+++ 05 - Dictionary and Sets/Prctice_Set.py
@@ -0,0 +1,23 @@
+# Problem 1. Write a program to create a dictionary of Hindi words with values as their English translation. 
+# Provide the user with an option to look it up! ?
+
+# myDict = {
+#     "पंखा": "Fan",
+#     "वास्तु" : "Item",
+#     " किताब ": "Books"
+# }
+# print("Options are", myDict.keys() )
+# a = input("Enter the Hindi Words is: \n")
+# print("The meaning of the word is :", myDict[a])
+
+# Problem 2. Write a program to input eight numbers from the user and display all the unique numbers (once).
+num1 = input("Enter number 1 \n")
+num2 = input("Enter number 2 \n")
+num3 = input("Enter number 3 \n")
+num4 = input("Enter number 4 \n")
+num5 = input("Enter number 5 \n")
+num6 = input("Enter number 6 \n")
+num7 = input("Enter number 7 \n")
+num8 = input("Enter number 8 \n")
+
+S = {num1,num2,num3,num4,num5,num6,num7,num8}
