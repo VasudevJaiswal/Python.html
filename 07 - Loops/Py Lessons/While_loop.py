5f8835faa54284e85e94bf3e26f844a95bb41762
Vasudev Jaiswal <vasujaiswal00@gmail.com>
2021-08-08 11:55:51 -0700

    Python.py

---
+++ 07 - Loops/Py Lessons/While_loop.py
@@ -0,0 +1,18 @@
+# While loop
+
+# The syntax of a while loop looks like this:
+
+''' while condition:
+    #Body of the loop '''
+
+# The block keeps executing until the condition is true/false
+# In while loops, the condition is checked first. If it evaluates to true, the body of the loop is executed, otherwise not!
+
+# If the loop is entered, the process of condition check and execution is continued until the condition becomes false.
+
+i = 0
+while i<10:
+    print("Yes" + str(i))
+    i = i+1
+
+print("Done")
